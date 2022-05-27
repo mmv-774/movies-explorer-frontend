e4761969cb71bf4a359c3bc9bc7849764c308762
MIkhail <mmv-774@yandex.ru>
2022-05-27 20:55:10 +0500

    компонент Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
+import Main from '../Main/Main';
+import Promo from '../Promo/Promo';
 import './App.css';
 
 const App = () => {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <Main>
+        <Promo />
+      </Main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,13 @@
+import './Promo.css';
+
+const Promo = () => {
+  return (
+    <div className='promo'>
+      <div className='promo_wrapper'>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Promo;
