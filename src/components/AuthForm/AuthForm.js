02dc4acc86e7167350334f5a444a843ebdc63e13
MIkhail <mmv-774@yandex.ru>
2022-06-03 17:32:00 +0500

    компонент AuthForm

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -5,24 +5,26 @@ import './AuthForm.css';
 
 const AuthForm = ({ title, submit, text, linkTo, linkText, children }) => {
   return (
-    <section className='auth-form'>
-      <div className='auth-form__header'>
-        <Link to={'/'} className='animated-link'>
-          <img src={logo} alt='Логотип' className='auth-form__logo' />
-        </Link>
-        <h2 className='auth-form__tittle'>{title}</h2>
+    <div className='auth-form'>
+      <div className='auth-form__wrapper'>
+        <div className='auth-form__header'>
+          <Link to={'/'} className='animated-link'>
+            <img src={logo} alt='Логотип' className='auth-form__logo' />
+          </Link>
+          <h2 className='auth-form__tittle'>{title}</h2>
+        </div>
+        <form className='auth-form__form'>
+          <div className='auth-form__inputs-container'>{children}</div>
+          <SubmitButton caption={submit} disabled={false} />
+        </form>
+        <div className='auth-form__info'>
+          <span className='auth-form__text'>{text}</span>
+          <Link to={linkTo} className='auth-form__link animated-link'>
+            {linkText}
+          </Link>
+        </div>
       </div>
-      <form className='auth-form__form'>
-        <div className='auth-form__inputs-container'>{children}</div>
-        <SubmitButton caption={submit} disabled={false} />
-      </form>
-      <div className='auth-form__info'>
-        <span className='auth-form__text'>{text}</span>
-        <Link to={linkTo} className='auth-form__link animated-link'>
-          {linkText}
-        </Link>
-      </div>
-    </section>
+    </div>
   );
 };
 
