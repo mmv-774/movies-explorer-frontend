161d60038683744648ad735f36473ad2be2820f4
MIkhail <mmv-774@yandex.ru>
2022-06-08 20:40:40 +0500

    создать CurrentUserContext
    глобальный стейт с информацией о пользователе

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
@@ -11,38 +13,42 @@ import NotFound from '../NotFound/NotFound';
 import './App.css';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = React.useState({});
+
   return (
     <div className='app'>
-      <Switch>
-        <Route exact path='/'>
-          <Header loggedIn={false} backgroundColor={'gray'} />
-          <Main />
-          <Footer />
-        </Route>
-        <Route path='/movies'>
-          <Header loggedIn={true} backgroundColor={'light'} />
-          <Movies />
-          <Footer />
-        </Route>
-        <Route path='/saved-movies'>
-          <Header loggedIn={true} backgroundColor={'light'} />
-          <SavedMovies />
-          <Footer />
-        </Route>
-        <Route path='/profile'>
-          <Header loggedIn={true} />
-          <Profile />
-        </Route>
-        <Route path='/signin'>
-          <Login />
-        </Route>
-        <Route path='/signup'>
-          <Register />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      <CurrentUserContext.Provider value={currentUser}>
+        <Switch>
+          <Route exact path='/'>
+            <Header loggedIn={false} backgroundColor={'gray'} />
+            <Main />
+            <Footer />
+          </Route>
+          <Route path='/movies'>
+            <Header loggedIn={true} backgroundColor={'light'} />
+            <Movies />
+            <Footer />
+          </Route>
+          <Route path='/saved-movies'>
+            <Header loggedIn={true} backgroundColor={'light'} />
+            <SavedMovies />
+            <Footer />
+          </Route>
+          <Route path='/profile'>
+            <Header loggedIn={true} />
+            <Profile />
+          </Route>
+          <Route path='/signin'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Register />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </CurrentUserContext.Provider>
     </div>
   );
 };
