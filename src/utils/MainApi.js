cd3e74c8fbbbf1dcbbf21ee163f7ff3e292fa1cd
MIkhail <mmv-774@yandex.ru>
2022-06-08 22:00:17 +0500

    создать функцию регистрации

---
+++ src/utils/MainApi.js
@@ -0,0 +1,23 @@
+export const BASE_URL = 'https://api.mm.nomoredomains.xyz';
+
+function _sendRequest(path, options) {
+  return fetch(`${this._baseUrl}${path}`, options).then((response) => _handleResponse(response));
+}
+
+function _handleResponse(response) {
+  const result = response.json();
+  if (response.ok) {
+    return result;
+  }
+  return result.then((res) => Promise.reject(res.message || res.error || 'Что-то пошло не так! Попробуйте еще раз.'));
+}
+
+export const register = (name, email, password) => {
+  return _sendRequest('/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+};
